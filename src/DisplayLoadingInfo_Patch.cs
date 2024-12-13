51ec4684c68b2c27fd588eaac817d4b94041ac54
t0stiman <18124323+t0stiman@users.noreply.github.com>
2024-12-13 21:41:22 +0100

    initial commit

---
+++ src/DisplayLoadingInfo_Patch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace no_mods_message;
+
+[HarmonyPatch(typeof(DisplayLoadingInfo))]
+[HarmonyPatch(nameof(DisplayLoadingInfo.OnLoadingStatusChanged))]
+public class DisplayLoadingInfo_OnLoadingStatusChanged_Patch 
+{
+	private static void Postfix(ref DisplayLoadingInfo __instance)
+	{
+		__instance.modsNoticeTMP.text = "";
+	}
+}
---
+++ src/Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using UnityModManagerNet;
+using HarmonyLib;
+
+namespace no_mods_message
+{
+	[EnableReloading]
+	static class Main
+	{
+		private static UnityModManager.ModEntry myModEntry;
+		private static Harmony myHarmony;
+
+		//===========================================
+
+		private static bool Load(UnityModManager.ModEntry modEntry)
+		{
+			try
+			{
+				myModEntry = modEntry;
+				myModEntry.OnUnload = OnUnload;
+				
+				myHarmony = new Harmony(myModEntry.Info.Id);
+				myHarmony.PatchAll(Assembly.GetExecutingAssembly());
+			}
+			catch (Exception ex)
+			{
+				myModEntry.Logger.LogException($"Failed to load {myModEntry.Info.DisplayName}:", ex);
+				myHarmony?.UnpatchAll(myModEntry.Info.Id);
+				return false;
+			}
+
+			myModEntry.Logger.Log("loaded");
+			return true;
+		}
+
+		private static bool OnUnload(UnityModManager.ModEntry modEntry)
+		{
+			myHarmony?.UnpatchAll(myModEntry.Info.Id);
+			return true;
+		}
+	}
+}
